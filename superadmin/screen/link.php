4ae6218dcd2e305c57ab76733ca33fabed5fda2e
retrodtech <tech@retrodtech.com>
2022-08-23 03:15:41 +0530

    update

--- superadmin/screen/link.php
+++
@@ -1,9 +0,0 @@
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
-  <link href="css/icons.css" rel="stylesheet">
-  <link href="css/svg.css" rel="stylesheet">
-  <link id="pagestyle" href="css/getbootstrap.css" rel="stylesheet">
-  <link id="pagestyle" href="css/style.css" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css">
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/themes/base/jquery-ui.min.css" rel="stylesheet">
-  <META HTTP-EQUIV="Pragma" CONTENT="no-cache">
-<META HTTP-EQUIV="Expires" CONTENT="-1">
--- superadmin/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-include ('include/constant.php');
-include (SERVER_INCLUDE_PATH.'db.php');
-include (SERVER_INCLUDE_PATH.'function.php');
-
-
-
-?>
